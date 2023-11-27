ddc201affd8a5cb4cee4afc1f90304971908000a
junilpyeon <dreamerjunil@gmail.com>
2023-11-27 20:29:58 +0900

    Init: 코틀린 스프링 커머스 웹개발 시작

---
+++ src/main/kotlin/com/example/kopring_game_commerce/KopringGameCommerceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kopring_game_commerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringGameCommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringGameCommerceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kopring_game_commerce/KopringGameCommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kopring_game_commerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringGameCommerceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
