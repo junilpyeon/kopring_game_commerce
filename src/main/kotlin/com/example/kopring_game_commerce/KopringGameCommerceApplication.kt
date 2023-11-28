de073be0f6644ed262417d8c48614d1e804dcfa4
junilpyeon <dreamerjunil@gmail.com>
2023-11-28 18:32:43 +0900

    Feat: Profile 설정하기

--- src/main/kotlin/com/example/kopring_game_commerce/KopringGameCommerceApplication.kt
+++ src/main/kotlin/com/example/kopring_game_commerce/KopringGameCommerceApplication.kt
@@ -1,10 +1,21 @@
 package com.example.kopring_game_commerce
 
+import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KopringGameCommerceApplication
+class KopringGameCommerceApplication {
+
+    @Value("\${my-app.welcome-message}")
+    val myAppWelcomeMessage: String = ""
+
+    @PostConstruct
+    fun printMyAppWelcomeMessage() {
+        println(myAppWelcomeMessage)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<KopringGameCommerceApplication>(*args)
