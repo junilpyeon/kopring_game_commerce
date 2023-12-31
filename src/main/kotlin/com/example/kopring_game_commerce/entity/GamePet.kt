a7d2ac1a0cecaedc95c0f6e665102cec1049f1b9
junilpyeon <dreamerjunil@gmail.com>
2023-12-31 11:14:59 +0900

    Feat: Entity명 수정

--- src/main/kotlin/com/example/kopring_game_commerce/entity/GamePet.kt
+++ src/main/kotlin/com/example/kopring_game_commerce/entity/GamePet.kt
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
 @Entity
-data class FeedingFood(
+data class GamePet(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
